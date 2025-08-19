e75ce49429f565ec1abf66732e49a2eebb1a3d5d
eko777e <replitserver445@gmail.com>
2025-08-19 23:40:24 +0400

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -1,17 +1,17 @@
 from pyrogram.types import InlineKeyboardButton
 import config
-from InflexMusic import app
+from AlexaMusic import app
 
 
 def start_panel(_):
     buttons = [
         [ 
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_GROUP),
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_5"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
         ]
     ]
@@ -22,20 +22,20 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
+                text=_["S_B_5"],
                 url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
             )
         ],
         [  
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+            InlineKeyboardButton(text=_["S_B_1"], callback_data="settings_back_helper"),
+            InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_13"], url=config.SUPPORT_GROUP),
+            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
         ],
     ]
     return buttons
