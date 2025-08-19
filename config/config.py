11ab7326827d8408bf59ac8f3ff980443f41dda4
Nicatcabbarov60 <96873048+Nicatcabbarov60@users.noreply.github.com>
2025-08-20 01:26:27 +0400

    config.py

--- config/config.py
+++ config/config.py
@@ -47,9 +47,9 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RespublicSupport")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Vssohbet")
 
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/RespublicSupport")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Vssohbet")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
 
