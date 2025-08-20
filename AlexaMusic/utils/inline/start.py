46155b5446e0905655952ad8f7a279301db2b8b3
eko777e <replitserver445@gmail.com>
2025-08-20 21:00:44 +0400

    Update start.py

--- AlexaMusic/utils/inline/start.py
+++ AlexaMusic/utils/inline/start.py
@@ -1,41 +1,81 @@
+# Copyright (C) 2025 by Alexa_Help @ Github
+# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
+
+"""
+TheTeamAlexa is a project of Telegram bots with variety of purposes.
+Copyright (c) 2021 ~ Present Team Alexa
+
+This program is free software: you can redistribute it and can modify
+as you want or you can collabe if you have new ideas.
+"""
+
+from typing import Union
 from pyrogram.types import InlineKeyboardButton
-import config
+from config import SUPPORT_CHANNEL, SUPPORT_GROUP, OWNER_ID
 from AlexaMusic import app
 
 
-def start_panel(_):
+def start_pannel(_):
     buttons = [
-        [ 
-            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_GROUP),
-        ],
         [
             InlineKeyboardButton(
-                text=_["S_B_5"], url=f"https://t.me/{app.username}?startgroup=true"
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?start=help",
             ),
-        ]
+            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+        ],
     ]
+    if SUPPORT_CHANNEL and SUPPORT_GROUP:
+        buttons.append(
+            [
+                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
+                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+            ]
+        )
+    else:
+        if SUPPORT_CHANNEL:
+            buttons.append(
+                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
+            )
+        if SUPPORT_GROUP:
+            buttons.append(
+                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
+            )
     return buttons
 
 
-def private_panel(_):
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
+        [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]
+    ]
+    if SUPPORT_CHANNEL and SUPPORT_GROUP:
+        buttons.append(
+            [
+                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
+                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+            ]
+        )
+    else:
+        if SUPPORT_CHANNEL:
+            buttons.append(
+                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
+            )
+        if SUPPORT_GROUP:
+            buttons.append(
+                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
+            )
+    buttons.append(
         [
             InlineKeyboardButton(
                 text=_["S_B_5"],
-                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
-        ],
-        [  
-            InlineKeyboardButton(text=_["S_B_1"], callback_data="settings_back_helper"),
-            InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_GROUP),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
-        ],
-    ]
+        ]
+    )
+    if OWNER_ID and OWNER:
+        buttons.append(
+            [
+                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER_ID),
+            ]
+        )
     return buttons
